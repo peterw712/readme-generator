10f78f595f5478962b3f20d2d1bf827364a3a150
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-04-19 13:15:19 -0400

    prompts

---
+++ README.md
@@ -0,0 +1,27 @@
+# Title 
+er
+## Description
+er
+## Badges
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Table of contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+## Installation 
+er
+## Usage  
+er
+## Tests  
+er
+## Contributing 
+er
+## License  
+MIT
+## Questions  
+GitHub: [er](https://github.com/er)
+Email: [er](mailto:er)
+  
---
+++ index.js
@@ -0,0 +1,104 @@
+//import packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+//each prompt
+const prompts = [
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: emailInput => {
+            if(emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+        validate: usernameInput => {
+            if(usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: projectInput => {
+            if(projectInput) {
+                return true;
+            } else {
+                console.log('Please enter the title of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project',
+        validate: projectInput => {
+               if(projectInput) {
+                  return true;
+              } else {
+                  console.log('Please enter a description of your project!');
+                  return false;
+              }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please enter instructions on how to install your project',
+        validate: installInput => {
+             if(installInput) {
+                return true;
+              } else {
+                   console.log('Please enter instructions to install your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How can your project be used?',
+        validate: installInput => {
+             if(installInput){
+                return true;
+              } else {
+                   console.log('Please enter how your project can be used!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can someone contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can your project be tested?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license',
+        choices: ['MIT', 'Apache License 2.0', 'Eclipse Public License 1.0', 'Mozilla Public License 2.0', 'No License']
+    },
+];
+  
+  
